a690430e41a1b15e642ce2466fe672dc810facb4
ispope45 <33889316+ispope45@users.noreply.github.com>
2021-07-14 09:39:46 +0900

    20210713 openpyxl Pillow study

--- TestDir/openpyxl_img_test.py
+++ TestDir/openpyxl_img_test.py
@@ -8,11 +8,11 @@ img = Image("C:\\Users\\Jungly\\Pictures\\중평초PoE01.JPG")
 # Pillow img width : openpyxl cell width = 200 : 25 / 8 : 1
 # Pillow img height : openpyxl cell height = 200 : 150 / 4 : 3
 
-img.width = 300
-img.height = 200
+img.width = 1000
+img.height = 1000
 
-ws.column_dimensions['A'].width = 37.5
-ws.row_dimensions[1].height = 150
+ws.column_dimensions['A'].width = 125
+ws.row_dimensions[1].height = 750
 
 ws.add_image(img, "A1")
 
